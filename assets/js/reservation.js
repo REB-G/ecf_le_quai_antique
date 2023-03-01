f6dd51eed57583cf8606089c8804ab01db60eafa
Rebecca GERREY <rebeccag@live.fr>
2023-03-01 14:54:44 +0100

    navbar.js + reservation.js created.

---
+++ assets/js/reservation.js
@@ -0,0 +1,2 @@
+const essai = document.querySelector('.test_JS form')
+console.log(essai)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,2 @@
-const navBar = document.querySelector('.header__container--nav');
-const navBtn = document.querySelector('.header__container--btn i');
-
-function toggleNav() {
-    navBtn.classList.toggle('fa-bars');
-    navBtn.classList.toggle('fa-times');
-    navBar.classList.toggle('header__container--nav');
-    navBar.classList.toggle('nav-active');
-}
-
-navBtn.addEventListener('click', function() {
-    toggleNav();
-});
+import './navbar'
+import './reservation'
